243cab5143e99233aae329805ca9884a95e8fcaf
battistaennio <ennio.battista99@gmail.com>
2024-06-26 16:11:09 +0200

    feat: "aggiungi" button add newTask

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //Stampare all’interno di una lista HTML un item per ogni todo.
 //Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 //Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+//Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 //inizializzazione vue
 const { createApp } = Vue;
@@ -8,6 +9,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            newTask: "",
             toDo: [
                 {
                     text: "fare passeggiata con il cane",
@@ -36,6 +38,11 @@ createApp({
 
         removeTask(indice) {
             this.toDo.splice(indice, 1);
-        }
+        },
+
+        addTask() {
+            this.toDo.unshift({text: this.newTask, done: false})
+        },
+
     }
 }).mount("#container")
